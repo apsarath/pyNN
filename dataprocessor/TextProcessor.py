19c521fdfb4a469233034422d0cb1f388bcc6d8e
Sarath Chandar <sarathcse2008@gmail.com>
2015-06-16 18:11:30 -0400

    TextProcessor added

---
+++ dataprocessor/TextProcessor.py
@@ -0,0 +1,54 @@
+__author__ = 'sanbilp'
+
+import codecs
+
+from nltk.tokenize import WordPunctTokenizer
+
+"""
+Given a list of words, this function converts it into a single string which can be written to the file.
+"""
+def listtostring(alist):
+    newline = ""
+    for word in alist:
+        newline+=word+" "
+    newline = newline.strip()
+    return newline+"\n"
+
+"""
+Does some basic text processing..
+1. Tokenizes the sentence
+2. lowercases the words (if low=True)
+3. Changes numeric to <number> (if num=True)
+"""
+def TextProcessor(src, tgt, low=True, num=True):
+
+    print "processing "+src
+    if low==True:
+        print "lowercasing.."
+    if num==True:
+        print "removing numeric.."
+
+    srcfile = codecs.open(src,"r","utf-8")
+    tgtfile = codecs.open(tgt,"w","utf-8")
+
+    word_punct_tokenizer = WordPunctTokenizer()
+
+    linecount=0
+    for line in srcfile:
+        linecount+=1
+        line = word_punct_tokenizer.tokenize(line)
+        if low==True:
+            for i in range(0,len(line)):
+                line[i] = line[i].lower()
+        if num==True:
+            for i in range(0,len(line)):
+                if line[i].isnumeric()==True:
+                    line[i] = "<number>"
+
+        tgtfile.write(listtostring(line))
+
+    srcfile.close()
+    tgtfile.close()
+    print "done processing "+str(linecount)+" lines!!"
+
+TextProcessor("../../../watson_data/input/RecAE/wiki/million-wiki.txt","../../../watson_data/input/RecAE/wiki/new-million-wiki.txt")
